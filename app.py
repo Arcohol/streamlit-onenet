c45297df0c0b60d6fe1a5377b06bcefbde940700
Tiantian Li <i@arcohol.com>
2023-12-12 03:25:15 +0800

    Fix glitches
    Add a slide bar for specifying data size

--- app.py
+++ app.py
@@ -7,28 +7,30 @@ st.title("OneNET Demo")
 st.header(":earth_asia: Overview (Latest Values)")
 
 col1, col2 = st.columns(2)
-col1.metric("Temperature", get_latest_datapoint("temp") + " °C")
-col2.metric("Humidity", get_latest_datapoint("hum") + " %")
+col1.metric("Temperature", str(get_latest_datapoint("temp")) + " °C")
+col2.metric("Humidity", str(get_latest_datapoint("hum")) + " %")
 
 st.header(":chart_with_upwards_trend: Charts")
 
 with st.container(border=True):
-    temp = get_datapoints("temp", limit=50)
+    size = st.slider("Data Size", 5, 100, 10)
+
+    temp = get_datapoints("temp", limit=size)
     c = (
         alt.Chart(temp)
         .mark_line(point=True)
         .encode(x="Time", y="Value", tooltip=["Time", "Value"])
         .interactive()
     )
-    st.subheader("Temperature")
+    st.subheader("Temperature (°C)")
     temp_chart = st.altair_chart(c, use_container_width=True)
 
-    hum = get_datapoints("hum", limit=50)
+    hum = get_datapoints("hum", limit=size)
     c = (
         alt.Chart(hum)
         .mark_line(point=True)
         .encode(x="Time", y="Value", tooltip=["Time", "Value"])
         .interactive()
     )
-    st.subheader("Humidity")
+    st.subheader("Humidity (%)")
     hum_chart = st.altair_chart(c, use_container_width=True)
